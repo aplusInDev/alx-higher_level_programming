fbda0561b1aa7deae5bcef013b7ff5d3341311b2
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-06-05 21:51:50 +0000

    task 10 done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
+/**
+ * check_cycle - checks cycle
+ * @list: giving list
+ *
+ * Return: 0 (success)
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list, *fast = list;
+	listint_t *slow = list, *fast = list;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-            return (1);
-    }
-    return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
 }

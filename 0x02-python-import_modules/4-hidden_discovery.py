a75a07701525f417b8ab79d0df69ace81a278d03
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-06-09 00:17:18 +0000

    test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# This is a program that prints all the names
-# defined by the compiled module hidden_4.pyc .
 
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for name in names:
-    if name[:2] != "__":
-    print(name)
+        if name[:2] != "__":
+            print(name)

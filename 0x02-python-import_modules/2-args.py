b36ed2df984a522eb086888298f9be90ec2ac20a
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-06-09 00:02:19 +0000

    upload task 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-from sys import argv
 if __name__ == "__main__":
-    size = len(argv)
+    from sys import argv
+
+    size = len(argv) - 1
 
     if size > 1:
-        print("{} arguments: ".format(size - 1))
-        for i in range(1, size):
-            print("{}: {}".format(i, arg[i]))
-    elif size - 1 == 0:
-        print("{} arguments.".format(size - 1))
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, argv[i]))
+    elif size == 0:
+        print("{} arguments.".format(size))
     else:
-        print("{} argument:".format(size - 1))
-        print("{}: {}".format(size - 1, arg[1]))
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, argv[1]))

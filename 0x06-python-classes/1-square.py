7f765c660954a6ceacdce57f92ba1a7d2f2a2502
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-06-29 22:27:43 +0100

    update task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" this module create Square class """
+
+
 class Square:
+    """ this is Square class """
+
     def __init__(self, size=0):
+        """ initialize size to square """
         self.__size = size

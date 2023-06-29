f611d9793f864042c96952e201cf235b7293eaf8
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-06-30 00:52:57 +0100

    update task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+""" this module create Square class """
+
+
 class Square:
+    """ Square class """
+
     def __init__(self, size=0):
+        """ initialize Square class """
         self.__size = size
 
     @property
     def size(self):
+        """ get Square class """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ set Square class """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,9 +25,11 @@ class Square:
             self.__size = value
 
     def area(self):
+        """ calculate Square class area """
         return self.__size ** 2
 
     def my_print(self):
+        """ print Square class """
         if self.__size == 0:
             print()
         for i in range(self.__size):

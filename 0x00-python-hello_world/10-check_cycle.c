a1dbe5db88953287cad81479111201ca5dced6ae
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-07-13 19:17:27 +0000

    all done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,22 @@
 #include "lists.h"
 
 /**
- * check_cycle - check if loop found in giving list
- * @list: giving list root
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list
+ * Return: 1 or 0
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp, *curr;
+	listint_t *single, *dble;
 
-	tmp = list;
-	while (tmp->next)
+	if (!list)
+		return (0);
+	for (single = dble = list; single && dble && dble->next;)
 	{
-		curr = tmp->next;
-		while (curr)
-		{
-			if (tmp == curr)
-				return (1);
-			curr = curr->next;
-		}
-		tmp = tmp->next;
+		single = single->next;
+		dble = dble->next->next;
+		if (single == dble)
+			return (1);
 	}
 	return (0);
 }

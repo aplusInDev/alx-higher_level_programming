df6cda3d5579321b3bdc1e16712dde3edeffe4a9
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-07-17 18:25:16 +0100

    update 2 for task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,7 @@ from sys import argv
 
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
-exist = load_from_json_file("add_item.json")
 exist = []
+exist.append(load_from_json_file("add_item.json"))
 exist.extend(argv[1:])
 save_to_json_file(exist, "add_item.json")

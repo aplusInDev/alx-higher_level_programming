3cf9e5c42eaf17f6340fdb068ead6888705b8993
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-12 03:00:22 +0100

    updating task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-console.log(`C is fun
-Python is cool
-JavaScript is amazing`);
+const languges = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+languges.forEach((line) => {
+  console.log(line);
+});
